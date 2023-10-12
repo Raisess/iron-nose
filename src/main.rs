15b3e4fca9bc030cf141a0c5ea76f99abd6b53da
Raisess <raisechif@gmail.com>
2023-10-12 10:38:32 -0300

    feat: add logger and search terms file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::fs;
+
+use env_logger;
+use log::{info, warn};
+use serde_json;
 use tokio;
+
 use iron_nose::common::env::Env;
 use iron_nose::q_bittorrent::QBittorrent;
 use iron_nose::torrent_sniff::TorrentSniff;
 
-const TEMP_SEARCH_TERM: &str = "Indiana Jones";
+const SEARCH_TERMS_FILE: &str = "terms.json";
 
 #[tokio::main]
 async fn main() {
+  env_logger::init();
+
+  info!(target: "QBittorrent", "Connecting to client...");
   let mut q_bittorrent = QBittorrent::new(
     &Env::get::<String>("Q_BITTORRENT_HOST", "192.168.3.10"),
     Env::get::<u16>("Q_BITTORRENT_PORT", "8080"),
@@ -17,14 +26,26 @@ async fn main() {
       &Env::get::<String>("Q_BITTORRENT_PASS", "adminadmin"),
     )
     .await;
+  info!(target: "QBittorrent", "Connected and logged in!");
 
   let torrent_sniff = TorrentSniff::new(
     &Env::get::<String>("TORRENT_SNIFF_HOST", "192.168.3.10"),
     Env::get::<u16>("TORRENT_SNIFF_PORT", "8090"),
   );
 
-  handle_download(&q_bittorrent, &torrent_sniff, TEMP_SEARCH_TERM.to_string())
-    .await;
+
+  match fs::read_to_string(SEARCH_TERMS_FILE) {
+    Ok(content) => {
+      let data = serde_json::from_str::<Vec<String>>(&content)
+        .unwrap_or(Vec::new());
+
+      for item in data {
+        handle_download(&q_bittorrent, &torrent_sniff, item)
+          .await;
+      }
+    },
+    Err(error) => panic!("Failed to read file: {}", error),
+  }
 }
 
 async fn handle_download(
@@ -32,15 +53,19 @@ async fn handle_download(
   torrent_sniff: &TorrentSniff,
   search_term: String,
 ) {
+  info!(target: "TorrentSniff", "Searching for: {}...", search_term);
   let torrents = torrent_sniff.search(&search_term).await;
+  info!(target: "TorrentSniff", "Search completed, found {} results!", torrents.len());
 
   for torrent in torrents {
     match torrent.magnet_links.get(0) {
       Some(magnet_link) => {
+        info!(target: "QBittorrent", "Adding {} for download...", torrent.name);
         q_bittorrent.add_torrent(&torrent.name, &magnet_link.link).await;
+        info!(target: "QBittorrent", "Successfully added {}", torrent.name);
         break;
       },
-      None => println!("No magnet link found for torrent: {}", torrent.name),
+      None => warn!("No magnet link found for torrent: {}", torrent.name),
     }
   }
 }
